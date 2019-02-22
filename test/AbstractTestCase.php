7d6386db3e47064b0ab952e44cee331d37bbe155
Hendrik Heneke <heneke@tanzsport.de>
2019-02-22 10:07:18 +0100

    Removed environment parsing from abstract test case.

--- test/AbstractTestCase.php
+++ test/AbstractTestCase.php
@@ -27,10 +27,6 @@ namespace Tanzsport\ESV\API;
 abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
 {
 
-	const DEFAULT_ENV = '.env.json';
-	const ESV_VERIFY_SSL = 'ESV_VERIFY_SSL';
-	const ESV_COMPRESS_ENABLED = 'ESV_COMPRESS_ENABLED';
-
 	/**
 	 * @var \Tanzsport\ESV\API\MockClient
 	 */
@@ -39,73 +35,10 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
 	public function setUp()
 	{
 		parent::setUp();
-
-		$envFile = $this->getEnvFile();
-		if ($envFile != null) {
-			$this->setEnv($envFile);
-		}
-
 		date_default_timezone_set('Europe/Berlin');
 
 		$this->client = new MockClient(
-			new Endpunkt($this->getEnv('ESV_ENDPOINT')), 'PHPUnit', $this->getEnv('ESV_TOKEN'),
-			$this->getEnv('ESV_USER'), $this->getEnv('ESV_PASSWORD'), $this->isCompressEnabled(), $this->isVerifySSL()
+			new Endpunkt('http://test'), 'PHPUnit', 'token', 'user', 'password'
 		);
-	}
-
-	protected function getEnvFile()
-	{
-		return __DIR__ . '/../' . self::DEFAULT_ENV;
-	}
-
-	protected function setEnv($envFile)
-	{
-		if (file_exists($envFile)) {
-			$objEnv = json_decode(file_get_contents($envFile));
-			$vars = array('ESV_ENDPOINT', 'ESV_TOKEN', 'ESV_USER', 'ESV_PASSWORD', self::ESV_COMPRESS_ENABLED, self::ESV_VERIFY_SSL);
-			foreach ($vars as $var) {
-				if (isset($objEnv->$var)) {
-					$_SERVER[$var] = $objEnv->$var;
-				}
-			}
-		}
-	}
-
-	protected function getEnv($var)
-	{
-		if (!$var) {
-			throw new \InvalidArgumentException('Variable erforderlich!');
-		}
-		if (isset($_SERVER[$var])) {
-			return $_SERVER[$var];
-		} else {
-			throw new \RuntimeException("Umgebungsvariable {$var} ist nicht definiert.");
-		}
-	}
-
-	protected function isVerifySSL()
-	{
-		if (isset($_SERVER[self::ESV_VERIFY_SSL])) {
-			if (is_bool($_SERVER[self::ESV_VERIFY_SSL])) {
-				return $_SERVER[self::ESV_VERIFY_SSL];
-			} else {
-				return $_SERVER[self::ESV_VERIFY_SSL] > 0;
-			}
-		} else {
-			return true;
-		}
-	}
-
-	protected function isCompressEnabled()
-	{
-		if (isset($_SERVER[self::ESV_COMPRESS_ENABLED])) {
-			if (is_bool($_SERVER[self::ESV_COMPRESS_ENABLED])) {
-				return $_SERVER[self::ESV_COMPRESS_ENABLED];
-			} else {
-				return $_SERVER[self::ESV_COMPRESS_ENABLED] > 0;
-			}
-		} else {
-			return true;
-		}
 	}
 }
