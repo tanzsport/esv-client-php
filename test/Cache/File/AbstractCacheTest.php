94d10a4bc60278f2679f93f2ec3d33b4d9f2b749
Hendrik Heneke <heneke@tanzsport.de>
2019-02-22 09:53:12 +0100

    Added abstract modifier.

--- test/Cache/File/AbstractCacheTest.php
+++ test/Cache/File/AbstractCacheTest.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace Tanzsport\ESV\API\Cache\File;
 
-class AbstractCacheTest extends \PHPUnit_Framework_TestCase
+abstract class AbstractCacheTest extends \PHPUnit_Framework_TestCase
 {
 
 	protected function getDirectory()
